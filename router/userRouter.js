4f1aa38f9f4db4a05cd8c76ac5e71319ba646550
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2022-10-31 22:22:15 +0530

    authorization middleware added

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,8 +1,11 @@
 const userCtrl = require("../controllers/userCtrl");
 const router=require("express").Router();
+const auth = require("../middleware/auth");
 
 router.post('/register',userCtrl.register);
+router.get("/getuser", auth, userCtrl.getUsers);
 router.post('/signin',userCtrl.signin);
 router.post('/logout',userCtrl.logout);
+router.get("/refresh_token", userCtrl.refreshToken);
 
 module.exports = router;
