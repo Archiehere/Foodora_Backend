8ce03fc3fa8c0fa006ef9ddd0cae5479474f866b
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2022-11-12 21:34:51 +0530

    bug fixed on seller route

--- router/foodRouter.js
+++ router/foodRouter.js
@@ -1,10 +1,11 @@
 const foodCtrl=require("../controllers/foodCtrl");
 const router=require("express").Router();
 const Upload = require('../middleware/upload');
+const auth = require("../middleware/auth");
 
 router.post('/register',foodCtrl.register);
-router.post('/restaurantregister',Upload.uploadImg.array('image',5),foodCtrl.registerrestaurant);
-router.post('/foodlisting',Upload.uploadImg.single('image'), foodCtrl.listfooditems);
+router.post('/restaurantregister',auth,Upload.uploadImg.array('image',5),foodCtrl.registerrestaurant);
+router.post('/foodlisting',auth,Upload.uploadImg.single('image'), foodCtrl.listfooditems);
 router.post('/verify/send',foodCtrl.sendOTP);
 router.post('/verify',foodCtrl.verify);
 router.post('/signin',foodCtrl.signin);
@@ -12,7 +13,7 @@ router.post('/signin',foodCtrl.signin);
 router.post('/forgot/send',foodCtrl.forgotsendOTP);
 router.post('/forgot/verify',foodCtrl.forgotverify);
 router.post('/forgot/reset',foodCtrl.resetpass);
-router.post('/sellerprofile',foodCtrl.sellerprofile);
+router.post('/sellerprofile',auth,foodCtrl.sellerprofile);
 module.exports=router;
 
 
